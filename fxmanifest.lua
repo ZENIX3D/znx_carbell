3e1a0a40f3c0dcdb267d94c91a9f757da8fb6cb5
ZENIX <zenix77dev@gmail.com>
2024-10-23 00:46:26 +0200

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
 fx_version 'adamant' game 'gta5' lua54 'yes'
 
-author 'zenix.77'
+author 'zenix.77' discord 'discord.gg/zenix77mod'
 
 client_script { '@es_extended/imports.lua', '@ox_lib/init.lua', 'config.lua', 'client/*.lua' }
